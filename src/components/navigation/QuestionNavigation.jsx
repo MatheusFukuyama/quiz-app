9331f9988c1ba2e8d37f2d30dfbc9e85961f34af
MatheusFukuyama <mama.fukuyama@gmail.com>
2022-09-23 15:10:31 -0300

    responsive

--- src/components/navigation/QuestionNavigation.jsx
+++ src/components/navigation/QuestionNavigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 import './QuestionNavigation.css'
 import { useSelector } from 'react-redux'
@@ -6,28 +6,25 @@ import store from '../../redux/store'
 import { handleIndexQuestionChange } from '../../redux/actions'
 
 export default function QuestionNavigation() {
-  const { choice } = useSelector(state => state)
+  const { choice, question_index } = useSelector(state => state)
   const indexes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-
-    useEffect(() => {
-      
-    }, [choice])
-
-    const handleClick = (e) => {
-      store.dispatch(handleIndexQuestionChange(e.target.value))
-    }
+  const [page, setPage] = useState(null)
 
 
-    return (
-      <nav>
-          <ul className='navigation rounded'>
-            {
-              indexes.map((name, index) => (
-                <li className='page' value={index} key={index} onClick={handleClick}>{name}</li>
-              ))
-            }
-              
-          </ul>
-      </nav>
-    )
+  const handleClick = (e) => {
+    store.dispatch(handleIndexQuestionChange(e.target.value))
+  }
+
+  return (
+    <nav>
+        <ul className='navigation rounded'>
+          {
+            indexes.map((name, index) => (
+                <li className="page" value={index} key={index} onClick={handleClick}>{name}</li>
+            ))
+          }
+            
+        </ul>
+    </nav>
+  )
 }
